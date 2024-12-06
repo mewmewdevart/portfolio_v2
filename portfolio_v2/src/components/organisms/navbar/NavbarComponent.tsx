fe89cf990a65b8eafcc52008a5127984894590e2
mewUbuntu <larissa@example.com>
2024-12-06 14:56:18 -0300

    feat: adjustmenets of the navbar desktop and mobile

--- portfolio_v2/src/components/organisms/navbar/NavbarComponent.tsx
+++ portfolio_v2/src/components/organisms/navbar/NavbarComponent.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import style from './NavbarComponent.module.css';
-import logo from '../../../assets/images/icons/logo.svg';
+import React, { useState } from "react";
+import style from "./NavbarComponent.module.css";
+import logo from "@assets/images/icons/logo.svg";
+
+import { BeakerIcon } from "@heroicons/react/24/solid";
 
 interface NavbarComponentProps {
-  navItems: { label: string, link: string }[];
+  navItems: { label: string; link: string }[];
 }
 
 const NavbarComponent: React.FC<NavbarComponentProps> = ({ navItems }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
   return (
     <nav className={style.navbar}>
-      <a className={style.navbar__logo} href='#'>
-        <img src={logo} alt="Logo MewmewDev" className='w-8 h-8' />
+      <a className={style.navbar__logo} href="#">
+        <img src={logo} alt="Logo MewmewDev" className="w-8 h-8" />
         MewmewDev
       </a>
       <ul className={style.navbar__list}>
@@ -23,8 +31,57 @@ const NavbarComponent: React.FC<NavbarComponentProps> = ({ navItems }) => {
           </li>
         ))}
         <li className={style.navbar__custom}>Currículo</li>
+        {/* <li className={style.navbar__item}>
+          <button
+            className={style.navbar__link}
+            onClick={toggleDropdown}
+            aria-haspopup="true"
+            aria-expanded={isDropdownOpen ? "true" : "false"}
+          >
+            Pt-BR{" "}
+            {isDropdownOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            )}
+          </button>
+          {isDropdownOpen && (
+            <ul className={style.navbar__dropdown}>
+              <li>
+                <a href="#" className={style.navbar__dropdownItem}>
+                  English
+                </a>
+              </li>
+            </ul>
+          )}
+        </li> */}
       </ul>
-
     </nav>
   );
 };
