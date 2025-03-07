15e31aa55e02ee6b69a98b35ea90fc5e8754301a
mewUbuntu <mewmewdevart@gmail.com>
2025-03-06 23:32:44 -0300

    feat: add the navbar modifications and styles

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -4,23 +4,36 @@ import HomePage from '@pages/HomePage/HomePage';
 import './index.css';
 import ProjectsPage from '@pages/ProjectsPage/ProjectsPage';
 import AboutMePage from '@pages/AboutMePage/AboutMePage';
+import ExperiencePage from '@pages/ExperiencePage/ExperiencePage';
 import useScrollNavigation from './hooks/useScrollNavigation'; 
 import { TranslationProvider } from './context/TranslationContext.tsx'
+import CursorGlow from '@atoms/CursorGlowComponent/CursorGlowComponent.tsx';
+import NavbarComponent from '@molecules/NavbarComponent/NavbarComponent.tsx';
+import useResponsiveness from './hooks/useResponsiveness.ts';
+import NavbarMobileComponent from '@molecules/NavbarMobileComponent/NavbarMobileComponent.tsx';
+import BackToTop from '@atoms/BackToTop/BackToTop.tsx';
 
 const App = () => {
-  useScrollNavigation();
+  // useScrollNavigation();
+
+  const isMobile = useResponsiveness();
 
   return (
     <>
+    {isMobile ? <NavbarMobileComponent/> : <NavbarComponent /> }
       <div id="home">
         <HomePage />
       </div>
       <div id="about">
         <AboutMePage />
       </div>
-      <div id="projects">
+      <div id="experience">
+        <ExperiencePage />
+      </div>
+      <div id="experience">
         <ProjectsPage />
       </div>
+      <BackToTop />
     </>
   );
 };
@@ -28,6 +41,7 @@ const App = () => {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TranslationProvider>
+    <CursorGlow />
       <App />
       </TranslationProvider>
   </StrictMode>
